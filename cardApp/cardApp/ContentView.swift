4da9c1e54695da1da59655fd6d6be9d4ee65044c
vladislavstr <58762058+vladislavstr@users.noreply.github.com>
2023-03-16 01:00:01 +0800

    style to text

--- cardApp/cardApp/ContentView.swift
+++ cardApp/cardApp/ContentView.swift
@@ -12,9 +12,17 @@ struct ContentView: View {
             ZStack {
                 Color(red: 0.91, green: 0.50, blue: 0.40).ignoresSafeArea()
                 VStack {
-                    Image("person-circle").resizable().aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/).frame(width: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/)
-                    Text("Name")
-                }
+                    Image("person-circle").resizable().aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/).frame(width: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/).clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/).overlay(Circle().stroke(lineWidth: 15))
+                    Text("first name")
+                        .font(.title)
+                        .fontWeight(.heavy)
+                    Text("last name")
+                        .font(.title)
+                        .fontWeight(.heavy)
+                    Text("position")
+                        .font(.headline)
+                        .fontWeight(.thin)
+                }.foregroundColor(.white)
                 
             }
     }
